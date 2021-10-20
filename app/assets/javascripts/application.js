95c0f75c1509b68b695888471b98ba1bd7559166
Megumu Suzuki <m.suzuki.te@gmail.com>
2021-10-20 06:24:04 +0000

    jQueryの学習

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,45 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require jquery
+//= require jquery_ujs
+
+$(function(){
+  $('.box1').slideDown(2000);
+});
+
+$(function(){
+  $('.box2').slideUp(1000);
+  $('.box2').show(1000);
+});
+
+$(function(){
+  $('.box3').show(1000);
+  $('.box3').css({'background-color': '#FF0'});
+});
+
+$(function(){
+  $('.box4').hide(1000);
+  $('.box4').slideDown(1000);
+});
+
+$(function(){
+  $('.box5').show(1000);
+  $('.box5').mouseover(function(){
+    $('.box5').addClass('box-over');
+  });
+  $('.box5').mouseout(function(){
+    $('.box5').removeClass('box-over');
+  });
+});
+
+$(function(){
+  $('.box6').hide(250);
+  $('.box6').show(1750);
+  $('.box6').on('click', function(){
+    $('.box6').addClass('box-click');
+  });
+  $('.box6').mouseout(function(){
+    $('.box6').removeClass('box-click');
+  });
+});
---
+++ app/assets/javascripts/sample.js
@@ -0,0 +1,21 @@
+// varによる再宣言
+var nickname = "taro"
+console.log(nickname)
+var nickname = "ichiro"
+console.log(nickname)
+// →再宣言可能
+
+// letによる再宣言
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+// →再宣言不可能
+
+// constによる再宣言
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+// →再宣言不可能
+
