c40eb91a52eee37682a3d431e2716747adec2bf6
Megumu Suzuki <m.suzuki.te@gmail.com>
2021-10-20 08:13:20 +0000

    画像スライド機能学習(Skippr)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
 //= require jquery
 //= require jquery_ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
 
 $(function(){
   $('.box1').slideDown(2000);
@@ -53,5 +54,46 @@ $(function(){
   });
   $('.box6').mouseout(function(){
     $('.box6').removeClass('box-click');
+  });
+});
+
+// 対象の要素のみを変更する
+$(function(){
+  $('.square1').on('click', function(){
+    $(this).slideUp();
+  });
+});
+
+// 子要素全てを変更する
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
+  });
+});
+
+
+
+$(document).ready(function () {
+  $("#images").skippr({
+    // スライドショーの変化 ("fade" or "slide")
+    transition : 'fade',
+    // 変化に係る時間(ミリ秒)
+    speed : 1000,
+    // easingの種類
+    easing : 'easeOutQuart',
+    // ナビゲーションの形("block" or "bubble")
+    navType : 'bubble',
+    // 子要素の種類("div" or "img")
+    childrenElementType : 'div',
+    // ナビゲーション矢印の表示(trueで表示)
+    arrows : true,
+    // スライドショーの自動再生(falseで自動再生なし)
+    autoPlay : true,
+    // 自動再生時のスライド切替間隔(ミリ秒)
+    autoPlayDuration : 2000,
+    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
+    keyboardOnAlways : true,
+    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
+    hidePrevious : false
   });
 });
--- app/assets/javascripts/function.js
+++ app/assets/javascripts/function.js
@@ -16,69 +16,69 @@
 // alert(promptStr);
 
 
-// ジャンケン
-let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
-// グー、チョキ、パー以外かつキャンセルじゃなかったら
-while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
-  alert('グー・チョキ・パーのいずれかを入力して下さい');
-  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
-}
+// // ジャンケン
+// let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+// // グー、チョキ、パー以外かつキャンセルじゃなかったら
+// while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+//   alert('グー・チョキ・パーのいずれかを入力して下さい');
+//   user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+// }
 
-// 相手の選んだ手
-let js_hand = getJShand();
+// // 相手の選んだ手
+// let js_hand = getJShand();
 
-// 勝敗結果
-let judge = winLose(user_hand, js_hand);
+// // 勝敗結果
+// let judge = winLose(user_hand, js_hand);
 
-// キャンセルでなかったら
-if (user_hand != null){
-  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
-} else {
-  alert("またチャレンジしてね")
-}
+// // キャンセルでなかったら
+// if (user_hand != null){
+//   alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+// } else {
+//   alert("またチャレンジしてね")
+// }
 
-// 相手が手を選び関数
-function getJShand(){
-  // Math.floor( Math.random() * 3 )は、0～2までのランダムな整数値を生成
-  let js_hand_num = Math.floor(Math.random() * 3);
-  let hand;
-  if (js_hand_num == 0){
-    hand = "グー";
-  } else if (js_hand_num == 1){
-    hand = "チョキ";
-  } else if (js_hand_num == 2){
-    hand = "パー";
-  }
-  return hand;
-}
+// // 相手が手を選び関数
+// function getJShand(){
+//   // Math.floor( Math.random() * 3 )は、0～2までのランダムな整数値を生成
+//   let js_hand_num = Math.floor(Math.random() * 3);
+//   let hand;
+//   if (js_hand_num == 0){
+//     hand = "グー";
+//   } else if (js_hand_num == 1){
+//     hand = "チョキ";
+//   } else if (js_hand_num == 2){
+//     hand = "パー";
+//   }
+//   return hand;
+// }
 
-// ジャンケンの勝敗を判定する関数
-function winLose(user, js){
-  let winLoseStr;
-  if (user == "グー"){
-    if (js == "グー"){
-      winLoseStr = "あいこ";
-    } else if (js == "チョキ"){
-      winLoseStr = "勝ち";
-    } else if (js == "パー"){
-      winLoseStr = "負け";
-    }
-  } else if (user == "チョキ"){
-    if (js == "グー"){
-      winLoseStr = "負け";
-    } else if (js == "チョキ"){
-      winLoseStr = "あいこ";
-    } else if (js == "パー"){
-      winLoseStr = "勝ち";
-    }
-  } else if (user == "パー"){
-    if (js == "グー"){
-      winLoseStr = "勝ち";
-    } else if (js == "チョキ"){
-      winLoseStr = "負け";
-    } else if (js == "パー"){
-      winLoseStr = "あいこ";
-    }
-  }
-  return winLoseStr;
-}
+// // ジャンケンの勝敗を判定する関数
+// function winLose(user, js){
+//   let winLoseStr;
+//   if (user == "グー"){
+//     if (js == "グー"){
+//       winLoseStr = "あいこ";
+//     } else if (js == "チョキ"){
+//       winLoseStr = "勝ち";
+//     } else if (js == "パー"){
+//       winLoseStr = "負け";
+//     }
+//   } else if (user == "チョキ"){
+//     if (js == "グー"){
+//       winLoseStr = "負け";
+//     } else if (js == "チョキ"){
+//       winLoseStr = "あいこ";
+//     } else if (js == "パー"){
+//       winLoseStr = "勝ち";
+//     }
+//   } else if (user == "パー"){
+//     if (js == "グー"){
+//       winLoseStr = "勝ち";
+//     } else if (js == "チョキ"){
+//       winLoseStr = "負け";
+//     } else if (js == "パー"){
+//       winLoseStr = "あいこ";
+//     }
+//   }
+//   return winLoseStr;
+// }
